b3af74b0d116119baacb8b75cef7ae6303826fb5
chenne19 <152810538+chenne19@users.noreply.github.com>
2024-06-19 14:38:42 -0400

    Sheet1 draft

---
+++ AlgebraInLean/Chapter0LeanIntroSOLUTIONS/Part2Solutions.lean
@@ -0,0 +1,8 @@
+/-
+This is a solutions doc.
+-/
+
+import Mathlib.Tactic
+
+
+/-Basic Tactics through Logic?-/
---
+++ AlgebraInLean/Chapter0LeanIntroSOLUTIONS/Part3Solutions.lean
@@ -0,0 +1,8 @@
+/-
+This is a solutions doc.
+-/
+
+import Mathlib.Tactic
+
+
+/-Overflow from Sheet 2? Also need to do Functions and Sets-/
