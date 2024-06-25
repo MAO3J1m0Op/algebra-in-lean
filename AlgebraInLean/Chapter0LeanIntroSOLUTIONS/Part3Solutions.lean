48070cf7a8dfed9c271ae559e7ca27783092f2b2
chenne19 <152810538+chenne19@users.noreply.github.com>
2024-06-25 15:05:47 -0400

    Transfer NNG exercises
    Natural number game exercises to Sheet 2.

--- AlgebraInLean/Chapter0LeanIntroSOLUTIONS/Part3Solutions.lean
+++ AlgebraInLean/Chapter0LeanIntroSOLUTIONS/Part3Solutions.lean
@@ -4,5 +4,8 @@ This is a solutions doc.
 
 import Mathlib.Tactic
 
-
 /-Overflow from Sheet 2? Also need to do Functions and Sets-/
+
+/-EDIT THIS: Credit for exercises goes to Kevin Buzzard and his Formalizing Mathematics course.
+If you would like to do some more practice and learn more about Lean, his course is
+a great place to start!-/
