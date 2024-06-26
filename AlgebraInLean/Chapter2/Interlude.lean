e1ebb696ac61772856b06bc2cfc7ce6e2e236146
Dominic King <djk59@duke.edu>
2024-06-26 14:12:22 -0400

    Changed up interlude sheet
    Added relevant examples to injectivity,
    surjectivity, and bijectivity.
    Trying to tie to together best with morphisms
    for the next sheet.
    
    Ensuring there is room for expansion for a check
    of the use of more tactics other than the basics
    -- waiting on Chapter 01 for that essentially --.

--- AlgebraInLean/Chapter2/Interlude.lean
+++ AlgebraInLean/Chapter2/Interlude.lean
@@ -33,25 +33,43 @@ namespace Interlude
   -- ## AS THE PROBLEM SETS DEVELOP, CAN ADD MORE PROBLEMS HERE TO
   -- ## CHECK PROFICIENCY IN EACH OF THE CRUCIAL ALGEBRA TACTICS
 
-  example {A B C : Type} (f : A → B) (g : B → C)
-  (h1: Injective f) (h2 : Injective g) : Injective (g ∘ f) := by
-    intros a1 a2 h
-    apply h1
-    apply h2
-    exact h
-    done
+  -- Let's prove a few basic consequences of function composition.
 
-  -- You just proved that given two injective maps `f, g`, the composition of
-  -- those maps `g ∘ f` is also injective.
+
+    -- This is simply restating the definition!
+    example (f : α → β) (h1 : Injective f) (h2 : Surjective f)
+    : (Bijective f) := by
+      sorry
+
+    -- Surjectivity composition
+    example (f : α → β) (g : β → γ) (h1: Surjective f) (h2 : Surjective g)
+    : Surjective (g ∘ f) := by
+      sorry
+
+    -- Injectivity composition
+    example (f : α → β) (g : β → γ) (h1: Injective f) (h2 : Injective g)
+    : Injective (g ∘ f) := by
+      intros a1 a2 h
+      apply h1
+      apply h2
+      exact h
+      done
+
+    -- Working backwards
+    example (f : α → β) (g : β → γ) (h1 : Injective (g ∘ f)) (h2 : Injective f)
+    : Injective g := by
+      sorry
+
+    -- Corrollary: bijectivity composition
+    example (f : α → β) (g : β → γ) (h1 : Bijective f) (h2 : Bijective g)
+    : Bijective (g ∘ f) := by
+      sorry
+
+
+  -- You just proved lots about two injective maps `f, g` and the composition
+  -- of those maps `g ∘ f` .
   -- Nice to see all those classic Lean tactics again, right?
-  -- Make sure we are fully comfortable with what we've seen above,
+  -- Make sure you are fully comfortable with what we've seen above,
   -- including `intros, apply, exact, done`. See Chapter 0 for a refresher.
-
-  example {A B C : Type} (f : A → B) (g : B → C)
-  (h1: Surjective f) (h2 : Surjective g) : Surjective (g ∘ f) := by
-    intros c
-    cases' h2 c with b hb
-    cases' h1 b with a ha
-    use a
-    rw [←hb, ←ha]
-    sorry
+  -- They are basic, but before moving on to the next Chapter, it is necessary
+  -- to be quite familiar with them.
