f9947615871b56d7a5752e5540c8b4869df2d96d
Stavan Jain <stavansjain@gmail.com>
2024-07-08 17:23:18 -0400

    moving all imports to AlgebraInLean.lean so that the relevant files are pre-built

--- AlgebraInLean.lean
+++ AlgebraInLean.lean
@@ -1,1 +1,6 @@
-import «AlgebraInLean».Basic
+import AlgebraInLean.Basic
+import AlgebraInLean.Chapter2.ModularArithmetic
+import AlgebraInLean.Chapter2.Sheet0
+import AlgebraInLean.Chapter2.Sheet1
+import AlgebraInLean.Chapter2.Sheet2
+import AlgebraInLean.Chapter2.Sheet3
