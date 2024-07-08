2b3d4cd2e00b22965e20b4021647b3e2166350d3
Stavan Jain <stavansjain@gmail.com>
2024-07-08 17:11:55 -0400

    Sheet 2 formatting

--- AlgebraInLean/Chapter2/Sheet2.lean
+++ AlgebraInLean/Chapter2/Sheet2.lean
@@ -1,32 +1,32 @@
-import «AlgebraInLean».Basic  -- ## FIX ME: IMPORT ISSUE (SHEET 3 TOO)
-import AlgebraInLean.Chapter2.Sheet1
 import Mathlib.Tactic
 
 namespace Defs
 
--- ## Isomorphisms
+/- ## Isomorphisms
 
--- In Mathlib, isomorphisms come with additional structure; they are not simply
--- defined as bijective homomorphisms.
+In Mathlib, isomorphisms come with additional structure; they are not simply
+defined as bijective homomorphisms.
 
--- They are defined as a structure, bundled up with some useful (and some
--- essential) fields:
--- - to_fun (a map from a group G → group H)
--- - inv_fun (a map from group H → group G)
--- - left_inv & right_inv (both inverses exist, thus a unique inverse exists)
--- - map_mul' (a proof of homomorphism/preservation of operation)
+They are defined as a structure, bundled up with some useful (and some
+essential) fields:
+- to_fun (a map from a group G → group H)
+- inv_fun (a map from group H → group G)
+- left_inv & right_inv (both inverses exist, thus a unique inverse exists)
+- map_mul' (a proof of homomorphism/preservation of operation)
 
--- So to prove an isomorphism, we have to provide proofs for each of these
--- fields.
+So to prove an isomorphism, we have to provide proofs for each of these
+fields.
 
--- Mathlib also lets us say that two groups are isomorphic by using the symbol
--- `≃+` for additive groups, and `≃*` for multiplicative groups.
+Mathlib also lets us say that two groups are isomorphic by using the symbol
+`≃+` for additive groups, and `≃*` for multiplicative groups.
 
--- Using this structure, we can come up with an arbitrary bijection and prove
--- that it is an isomorphism (or not), like in the trivial example below. The
--- identity map that takes each element of the additive group of integers to
--- itself is clearly an isomorphism. In fact, as you might've guessed, this
--- holds for any arbitrary group.
+Using this structure, we can come up with an arbitrary bijection and prove
+that it is an isomorphism (or not), like in the trivial example below. The
+identity map that takes each element of the additive group of integers to
+itself is clearly an isomorphism. In fact, as you might've guessed, this
+holds for any arbitrary group.
+
+-/
 
 -- Example, not exercise
 example (φ : ℤ → ℤ) (h1 : ∀ x, φ x = x) : ℤ ≃+ ℤ := by
@@ -49,12 +49,16 @@ example (φ : ℤ → ℤ) (h1 : ∀ x, φ x = x) : ℤ ≃+ ℤ := by
   rw [hc]
   simp [h1]
 
--- What about the bijection that takes each member of the additive group of
--- integers to its negative? Is this an isomorphism? This proof will be
--- extremely similar to the example above.
+/-
 
--- Since we are using integers in this proof, you might find the tactic
--- `linarith` helpful.
+What about the bijection that takes each member of the additive group of
+integers to its negative? Is this an isomorphism? This proof will be
+extremely similar to the example above.
+
+Since we are using integers in this proof, you might find the tactic
+`linarith` helpful.
+
+-/
 
 -- Exercise
 example (φ : ℤ → ℤ) (h1 : ∀ x, φ x = -x) : ℤ ≃+ ℤ := by
@@ -76,11 +80,15 @@ example (φ : ℤ → ℤ) (h1 : ∀ x, φ x = -x) : ℤ ≃+ ℤ := by
   simp [h1]
   linarith
 
--- Optional: Try choosing a group and a bijection, and prove that the map is an
--- isomorphism (or conversely, prove that it is not an isomorphism). Fill in
--- the underscores with your preferred groups, bijection, and proofs. An
--- example might be a map from the group of integers to itself, the map being
--- the function that takes an integer x to 2x.
+/-
+
+Optional: Try choosing a group and a bijection, and prove that the map is an
+isomorphism (or conversely, prove that it is not an isomorphism). Fill in
+the underscores with your preferred groups, bijection, and proofs. An
+example might be a map from the group of integers to itself, the map being
+the function that takes an integer x to 2x.
+
+-/
 
 example (φ : _ → _) (h1 : ∀ _) : _ ≃_ _ := by
   let hom_map : _ ≃ _ := by
@@ -94,9 +102,13 @@ example (φ : _ → _) (h1 : ∀ _) : _ ≃_ _ := by
   }
   sorry
 
--- Group isomorphisms from a group to itself (like the first example we looked
--- at) might seem uninteresting at first. Any group will have at least one
--- isomorphism associated with it: the identity map. Boring. However, there are
--- much more interesting examples of nontrivial group-to-itself isomorphisms,
--- which you will focus on next chapter; we'll also be pivoting back to our
--- implementation of morphisms.
+/-
+
+Group isomorphisms from a group to itself (like the first example we looked
+at) might seem uninteresting at first. Any group will have at least one
+isomorphism associated with it: the identity map. Boring. However, there are
+much more interesting examples of nontrivial group-to-itself isomorphisms,
+which you will focus on next chapter; we'll also be pivoting back to our
+implementation of morphisms.
+
+-/
