6ae6a60e9849bb987a69422283e545f0cccc09e0
Will Harris <willharris0407@gmail.com>
2024-07-09 19:32:31 -0400

    convert `Sn` to using `Equiv`
    Also include an example of proving S3 is non-abelian.

--- AlgebraInLean/Chapter01/Sheet07.lean
+++ AlgebraInLean/Chapter01/Sheet07.lean
@@ -3,105 +3,159 @@ import AlgebraInLean.Chapter01.Sheet06
 namespace AlgebraInLean
 
 /-
-## Challenge Sheet
+The last group we will cover in this section is the symmetric group. This is defined as the set of
+all permutatins of a set of n elements. This is formalized as the set of all bijections of a finite
+set of size n onto itself. The group operation, therefore, is function composition.
 
-The last groups we will cover in this chapter are the symmetric groups. This is defined by the
-different ways of permuting n elements. The group operation is composition. For example, S₃ has 6
-elements, which permute (1, 2, 3) into (1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), and
-(3, 2, 1). Elements in Sₙ can also be thought of as bijections (invertible functions) from a set of
-n elements to itself.
+For example, S3 has 6 elements, which permute the tuple (1, 2, 3) into one of: (1, 2, 3), (1, 3, 2),
+(2, 1, 3), (2, 3, 1), (3, 1, 2), or (3, 2, 1).
 
-We define Sₙ as a function from Fin n to Fin n, along with a proof that it is bijective (note that
-the group structure of Fin n is not relevant here). This is done with Lean `Subtype`s: for an
-arbitrary type `a` and predicate `p : α → Prop`, `{x : α // p x}` is the subtype of `α` satisfying
-`p`, like with set builder notation (note that `Set`s are also present in Lean, and they are similar
-to but distinct from subtypes). This might remind you of `∃ (x : α), p x`, and while they are
-related, objects of a subtype must actually include an `x`, while objects of the latter need only
-include a proof that `x` exists (which can be converted to the other using the axiom of choice).
+In Lean, we use the `Equiv` type, notated `α ≃ β`, which is the set of all bijections from a type
+`α` to a type `β`.
 -/
+def Sn (n : ℕ) : Type := Fin n ≃ Fin n
 
-/-- A symmetric group is a bijection Fin n → Fin n -/
-def Symmetric (n : ℕ) : Type := {f : Fin n → Fin n // Function.Bijective f}
-
-variable {n : ℕ}
-
+instance (n : ℕ) : Group (Sn n) where
+  op := Equiv.trans
+  op_assoc := λ _ _ _ ↦ rfl
+  id := Equiv.refl (Fin n)
+  op_id := λ _ ↦ rfl
+  id_op := λ _ ↦ rfl
+  inv := Equiv.symm
+  inv_op := by
+    intro f
+    dsimp [μ, 𝕖]
+    rw [←Equiv.coe_inj]
+    funext x
+    rw [Equiv.symm_trans_self, Equiv.refl_apply]
+    done
 
 /--
-Two elements of the same symmetric group are equal when the underlying functions are the same.
+This is a type similar in structure to `Fin 3`, but is easier to work with for the proof below.
 -/
-@[ext] -- this allows the "ext" tactic to work on elements of Sₙ
-theorem Symmetric.ext {n : ℕ} {f g : Symmetric n} (h : f.val = g.val) : f = g := by
-  unfold Symmetric at *
-  ext : 1  -- hover over `ext` to see the documentation that explains this syntax
-  exact h
-  done
-
-
-/--
-The group operation is function composition, along with a proof that the composition of two
-bijective functions is also bijective.
+inductive Three where
+| one   : Three
+| two   : Three
+| three : Three
+/-
+The line below enables the use of the `decide` tactic for closing goals involving the equality (or
+inequality) of our `Three` type.
 -/
-protected def Symmetric.op (f g : Symmetric n) : Symmetric n where
-  val := f.val ∘ g.val
-  property := Function.Bijective.comp f.prop g.prop
-
-
-/-- The inverse of an element of Sₙ is just the inverse of the underlying function. -/
-protected noncomputable def Symmetric.inv (f : Symmetric n) : Symmetric n := by
-  -- `choose` invokes the axiom of choice (see `Classical.choose` and `Classical.choose_spec`)
-  choose g h using Function.bijective_iff_has_inverse.mp f.prop
-  use g
-  apply Function.bijective_iff_has_inverse.mpr
-  use f.val
-  exact h.symm
-  done
+deriving DecidableEq
 
 /-
-This theorem allows for easier use of `.inv` defined above so you do not have to dig into how the
-`choose` tactic works.
+Here, we construct a bijection between our custom `Three` type and `Fin 3`.
 -/
-protected theorem Symmetric.inv_spec (f : Symmetric n)
-  : Function.LeftInverse f.inv.val f.val ∧ Function.RightInverse f.inv.val f.val :=
-  Classical.choose_spec (Function.bijective_iff_has_inverse.mp f.prop)
+def three_eq : Three ≃ Fin 3 where
+  toFun x := match x with
+  | Three.one => 0
+  | Three.two => 1
+  | Three.three => 2
+  invFun x := match x with
+  | 0 => Three.one
+  | 1 => Three.two
+  | 2 => Three.three
+  left_inv := by
+    intro x
+    cases x <;> rfl
+  right_inv := by
+    intro x
+    dsimp only
+    split <;> case _ heq =>
+      split at heq
+      all_goals (first | rfl | contradiction)
 
-
-noncomputable instance (n : ℕ) : Group (Symmetric n) where
-  op := Symmetric.op
-
-  op_assoc a b c := by
-    -- sorry
-    -- SAMPLE SOLUTION
-    rfl
-    -- END OF SAMPLE SOLUTION
-
-  id := by
-    use id
-    exact Function.bijective_id
-
-  op_id a := by
-    -- sorry
-    -- SAMPLE SOLUTION
-    rfl
-    -- END OF SAMPLE SOLUTION
-
-  id_op a := by
-    -- sorry
-    -- SAMPLE SOLUTION
-    rfl
-    -- END OF SAMPLE SOLUTION
-
-  inv := Symmetric.inv
+open Three in
+theorem S3_non_abelian : ¬∀ (f g : Three ≃ Three), f.trans g = g.trans f := by
 
   /-
-  Proving this is more involved than the others. If you get stuck, try using the
-  `Symmetric.inv_spec` theorem, the `ext` tactic, and looking at the proofs in the other sheets for
-  help.
+  Recall that the classical way to disprove a for-all statement is to construct a counterexample.
+  Here, we will guide you through the construction of a counterexample.
+
+  Fill in the `sorry`s to construct specific functions that demonstrate that S3 is non-abelian.
   -/
-  inv_op a := by
-    -- sorry
-    -- SAMPLE SOLUTION
-    ext : 1
-    simp only [μ, Symmetric.op, 𝕖, Symmetric.inv]
-    apply Function.LeftInverse.comp_eq_id
-    exact a.inv_spec.left
-    -- END OF SAMPLE SOLUTION
+  let f : Three → Three := λ x ↦ match x with
+  | one   => one
+  | two   => three
+  | three => two
+  let f_inv : Three → Three := λ x ↦ match x with
+  | one   => one
+  | two   => three
+  | three => two
+
+  let g : Three → Three := λ x ↦ match x with
+  | one   => two
+  | two   => one
+  | three => three
+  let g_inv : Three → Three := λ x ↦ match x with
+  | one   => two
+  | two   => one
+  | three => three
+
+  /-
+  The next two blocks construct `Equiv` instances from the four functions you constructed above.
+
+  Don't worry about the `sorry` declarations here; they're just there as fallback for Lean before
+  you have completed the function calls above.
+  -/
+  let f_eq : Three ≃ Three := {
+    toFun := f
+    invFun := f_inv
+    left_inv := by
+      intro x
+      dsimp [f, f_inv]
+      split <;> case _ heq =>
+        split at heq
+        all_goals (first | rfl | contradiction | sorry)
+    right_inv := by
+      intro x
+      dsimp [f, f_inv]
+      split <;> case _ heq =>
+        split at heq
+        all_goals (first | rfl | contradiction | sorry)
+  }
+
+  let g_eq : Three ≃ Three := {
+    toFun := g
+    invFun := g_inv
+    left_inv := by
+      intro x
+      dsimp [g, g_inv]
+      split <;> case _ heq =>
+        split at heq
+        all_goals (first | rfl | contradiction | sorry)
+    right_inv := by
+      intro x
+      dsimp [g, g_inv]
+      split <;> case _ heq =>
+        split at heq
+        all_goals (first | rfl | contradiction | sorry)
+  }
+
+  /-
+  Here, we extract all the definitons, and convert the negated ∀ into a ∃ counterexample. The goal
+  state after this block will look rather heinous, but trust that it has been placed in a format
+  that will allow Lean to cleanly close the goal (by way of the `decide` tactic) once you pick a
+  specific point of failure for the equality.
+  -/
+  apply not_forall_of_exists_not
+  use f_eq
+  apply not_forall_of_exists_not
+  use g_eq
+  rw [←Equiv.coe_inj]
+  simp [f_eq, f, g_eq, g, Eq.trans, Function.comp_apply]
+  rw [Function.funext_iff]
+  apply not_forall_of_exists_not
+
+  /-
+  Finally, pick a specific function value where the equality fails with the `use` tactic, and then
+  use the `decide` tactic to close the goal!
+
+  Before you move on, though, check that the `sorry` warning has disappeared from this lemma. If
+  it's still there, double check your functions, as that means the `Equiv` creation blocks are
+  failing because the functions you constructed are not bijective.
+  -/
+
+  -- EXERCISE
+  use three
+  decide
