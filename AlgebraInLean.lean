3a1255e0bb3e15c47cb65e52c72ed7c523997ec6
chenne19 <152810538+chenne19@users.noreply.github.com>
2024-07-09 15:07:33 -0400

    add chapter 0
    This commit is a squash merge of #15.
    
    This commit fails the `build-strict` test. This is because Lean 4.9.0
    introduced a linter that produces warnings upon the use of tactics that
    do not do anything, which flags our pedagogical inclusion of the `done`
    tactic. We'll fix this later.

--- AlgebraInLean.lean
+++ AlgebraInLean.lean
@@ -1,1 +1,7 @@
 import AlgebraInLean.Basic
+import AlgebraInLean.Chapter00.Sheet01
+import AlgebraInLean.Chapter00.Sheet02
+import AlgebraInLean.Chapter00.Sheet03
+import AlgebraInLean.Chapter00.Sheet04
+import AlgebraInLean.Chapter00.Sheet05
+import AlgebraInLean.Chapter00.Sheet06
