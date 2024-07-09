7b1eb74214bc3f1097cec8e689d5b982444e8e49
Stavan Jain <stavansjain@gmail.com>
2024-07-09 14:14:47 -0400

    Edits to interlude
    Only small edits

--- AlgebraInLean/Chapter2/ModularArithmetic.lean
+++ AlgebraInLean/Chapter2/ModularArithmetic.lean
@@ -6,10 +6,10 @@ namespace Nat
 
 /-
 
-From the output of `#check Nat.gcd`, we can see that the _greatest common
-denominator_ function takes in two natural numbers and outputs the largest
-natural number that will divide both inputs. Particularly, the gcd of any
-number and 0 is the number itself.
+From the type signature obtained by `#check Nat.gcd`, we can see that the _greatest common
+denominator_ function in mathlib takes in two natural numbers and outputs the largest natural number
+that will divide both inputs (ℕ → ℕ → ℕ). Particularly, the gcd of any number and 0 is the number
+itself.
 
 -/
 #check Nat.gcd
@@ -24,10 +24,9 @@ number and 0 is the number itself.
 
 /-
 
-Similarly, we define the _least common multiple_. It is a function which
-takes in two natural numbers and outputs the minimal natural number that is
-divisible by both inputs. The lcm of any number and 9 is 0, since 0 divided
-by any number is 0.
+Similarly, we define the _least common multiple_. It is a function which takes in two natural
+numbers and outputs the minimal natural number that is divisible by both inputs. The lcm of any
+number and 9 is 0, since 0 divided by any number is 0.
 
 -/
 #check lcm
@@ -48,50 +47,43 @@ variable (n : Nat)
 
 Note that the definition for lcm uses the gcd.
 
-Efficiently computing the gcd is a seemingly mundane, boring problem, but
-its implications quite literally form the backbone of the internet as we
-know it today.
+Efficiently computing the gcd is a seemingly mundane, boring problem, but its implications quite
+literally form the backbone of the internet as we know it today.
 
-The "naive" way to do it is through _prime factorization_; break up each of
-the numbers into their constituent, atomic parts, and then find the largest
-part they have in common. But this brute-force algorithm scales very poorly
-for large numbers, a performance bottleneck that cryptographic schemes like
-RSA depend on.
+The "naive" way to do it is through _prime factorization_; break up each of the numbers into their
+constituent, atomic parts, and then find the largest part they have in common. But this brute-force
+algorithm scales very poorly for large numbers, a performance bottleneck that cryptographic schemes
+like RSA depend on.
 
-Thankfully, there is a quicker way to find the gcd (and therefore lcm) via
-the Euclidean Algorithm.
+Thankfully, there is a quicker way to find the gcd (and therefore lcm) via the Euclidean Algorithm.
 
-To introduce the Euclidean Algorithm, first we have to cover modular
-arithmetic. Basic number theory might seem completely separate from abstract
-algebra at first, but it shows up increasingly in areas like ring theory, so
-bear with us.
+To introduce the Euclidean Algorithm, first we have to cover modular arithmetic. Basic number theory
+might seem completely separate from abstract algebra at first, but it shows up increasingly in areas
+like ring theory, so bear with us.
 
 ### Congruence modulo some integer `m`
 
-Given two integers `a` and `b`, with `b` non-zero, there exist unique
-integers `q` and `r` such that:
+Given two integers `a` and `b`, with `b` non-zero, there exist unique integers `q` and `r` such
+that:
 
 a = bq + r, where 0 ≤ r < |b|.
 
-In other words, we can write an integer a as a multiple of some other
-integer `b`, plus a positive remainder `r` with `r` strictly less than `b`.
-(What would happen if `r` were equal to or greater than `b`?)
+In other words, we can write an integer a as a multiple of some other integer `b`, plus a positive
+remainder `r` with `r` strictly less than `b`. (What would happen if `r` were equal to or greater
+than `b`?)
 
-When we talk about some integer `n` modulo `q`, we are simply disregarding
-the first term in the sum, `bq`, and only considering the remainder `r`.
+When we talk about some integer `n` modulo `q`, we are simply disregarding the first term in the
+sum, `bq`, and only considering the remainder `r`.
 
-A common real-world example is the analog clock. The clock runs modulo 12;
-once we get to the 12th hour, we "roll over" and start at 1 again. Thus, 13
-modulo 12 is 1. Another way to look at it is that 13 divided by 12 is 1 with
-remainder 1.
+A common real-world example is the analog clock. The clock runs modulo 12; once we get to the 12th
+hour, we "roll over" and start at 1 again. Thus, 13 modulo 12 is 1. Another way to look at it is
+that 13 divided by 12 is 1 with remainder 1.
 
-Another useful and small piece of notation is the vertical bar `∣`, not to
-be confused with the "pipe" operator `|` on your keyboard. You can write it
-as `\mid`.
+Another useful and small piece of notation is the vertical bar `∣`, not to be confused with the
+"pipe" operator `|` on your keyboard. You can write it as `\mid`.
 
-Given two integers `a` and `b`, `a ∣ b` simply means that `a` divides `b`,
-or `a` is a factor of `b`. `a ∤ b` means the inverse; `a` does not divide
-`b`.
+Given two integers `a` and `b`, `a ∣ b` simply means that `a` divides `b`, or `a` is a factor of
+`b`. `a ∤ b` means the inverse; `a` does not divide `b`.
 
 -/
 #eval Nat.mod 4 5
@@ -116,7 +108,7 @@ notation:50  a " ≡ " b "(mod " m ")" => congr_mod m a b
 /-
 
 Perhaps you would expect a + b (mod m) to equal (a mod m) + (b mod m).
-Similarly, a * b (mod m) does not simply equal (a mod m) * (b mod m). Why do
+Similarly, you might expect a * b (mod m) = (a mod m) * (b mod m). Why do
 we need the extra (mod m) at the end? We leave this as a (hopefully)
 thought-provoking exercise to the reader.
 
