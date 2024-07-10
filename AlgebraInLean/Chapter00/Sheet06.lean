3cafde19fe45a77833566529db90c624075787e8
chenne19 <152810538+chenne19@users.noreply.github.com>
2024-07-10 14:28:07 -0400

    fix typos and build strict fail
    * Fixes instances in Chapter 00 which refer to "chapters" as "worlds"
    * Fixes grammatical typos 
    * Disables `unusedTactic` linter in each sheet to suppress warnings
      emitted by the `done` tactic
    
    
    Co-authored-by: Will Harris <willharris0407@gmail.com>

--- AlgebraInLean/Chapter00/Sheet06.lean
+++ AlgebraInLean/Chapter00/Sheet06.lean
@@ -5,8 +5,8 @@ This is a solutions sheet.
 /-
 Tactics!
 
-Below is a list of tactics mentioned in World 0. Feel free to use this list for reference to write
-notes, or keep updating it with tactics mentioned in future worlds.
+Below is a list of tactics mentioned in Chapter 0. Feel free to use this list for reference to write
+notes, or keep updating it with tactics mentioned in future chapters.
 
 This cheatsheet from Formalising Mathematics might also be helpful:
 <https://www.ma.imperial.ac.uk/~buzzard/xena/formalising-mathematics-2024/Part_C/cheatsheet.html>
