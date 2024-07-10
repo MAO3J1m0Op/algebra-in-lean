79e361274f4d209fd098c672af3f03f01004128c
Will Harris <willharris0407@gmail.com>
2024-07-09 20:02:35 -0400

    oops; delete sheet 6 from `AlgebraInLean.lean`

--- AlgebraInLean.lean
+++ AlgebraInLean.lean
@@ -4,4 +4,3 @@ import AlgebraInLean.Chapter03.Sheet02
 import AlgebraInLean.Chapter03.Sheet03
 import AlgebraInLean.Chapter03.Sheet04
 import AlgebraInLean.Chapter03.Sheet05
-import AlgebraInLean.Chapter03.Sheet06
